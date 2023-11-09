ca9e4b1df590eb7e25f552fc135bbecff9b547c4
brooklynnsilva <brooklynnsilva@gmail.com>
2023-11-08 17:13:12 -0700

    Design complete with names and text.

--- PigLatinTranslator/Form1.Designer.cs
+++ PigLatinTranslator/Form1.Designer.cs
@@ -30,8 +30,11 @@
         {
             label1 = new Label();
             label2 = new Label();
-            textBox2 = new TextBox();
-            textBox1 = new TextBox();
+            txtPigLatin = new TextBox();
+            txtEnglish = new TextBox();
+            btnTranslate = new Button();
+            btnExit = new Button();
+            btnClear = new Button();
             SuspendLayout();
             // 
             // label1
@@ -48,39 +51,70 @@
             label2.AutoSize = true;
             label2.Location = new Point(27, 177);
             label2.Name = "label2";
-            label2.Size = new Size(127, 15);
+            label2.Size = new Size(115, 15);
             label2.TabIndex = 1;
-            label2.Text = "Enter English text here:";
+            label2.Text = "Pig Latin translation:";
             // 
-            // textBox2
+            // txtPigLatin
             // 
-            textBox2.Location = new Point(27, 195);
-            textBox2.Multiline = true;
-            textBox2.Name = "textBox2";
-            textBox2.ScrollBars = ScrollBars.Vertical;
-            textBox2.Size = new Size(456, 102);
-            textBox2.TabIndex = 3;
+            txtPigLatin.Location = new Point(27, 195);
+            txtPigLatin.Multiline = true;
+            txtPigLatin.Name = "txtPigLatin";
+            txtPigLatin.ReadOnly = true;
+            txtPigLatin.ScrollBars = ScrollBars.Vertical;
+            txtPigLatin.Size = new Size(456, 102);
+            txtPigLatin.TabIndex = 3;
             // 
-            // textBox1
+            // txtEnglish
             // 
-            textBox1.Location = new Point(27, 43);
-            textBox1.Multiline = true;
-            textBox1.Name = "textBox1";
-            textBox1.ScrollBars = ScrollBars.Vertical;
-            textBox1.Size = new Size(456, 102);
-            textBox1.TabIndex = 4;
+            txtEnglish.Location = new Point(27, 43);
+            txtEnglish.Multiline = true;
+            txtEnglish.Name = "txtEnglish";
+            txtEnglish.ScrollBars = ScrollBars.Vertical;
+            txtEnglish.Size = new Size(456, 102);
+            txtEnglish.TabIndex = 4;
+            // 
+            // btnTranslate
+            // 
+            btnTranslate.Location = new Point(27, 329);
+            btnTranslate.Name = "btnTranslate";
+            btnTranslate.Size = new Size(75, 23);
+            btnTranslate.TabIndex = 5;
+            btnTranslate.Text = "&Translate";
+            btnTranslate.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(408, 329);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(75, 23);
+            btnExit.TabIndex = 6;
+            btnExit.Text = "E&xit";
+            btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(145, 329);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(75, 23);
+            btnClear.TabIndex = 7;
+            btnClear.Text = "&Clear";
+            btnClear.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(520, 379);
-            Controls.Add(textBox1);
-            Controls.Add(textBox2);
+            ClientSize = new Size(516, 379);
+            Controls.Add(btnClear);
+            Controls.Add(btnExit);
+            Controls.Add(btnTranslate);
+            Controls.Add(txtEnglish);
+            Controls.Add(txtPigLatin);
             Controls.Add(label2);
             Controls.Add(label1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Pig Latin Translator";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -89,7 +123,10 @@
 
         private Label label1;
         private Label label2;
-        private TextBox textBox2;
-        private TextBox textBox1;
+        private TextBox txtPigLatin;
+        private TextBox txtEnglish;
+        private Button btnTranslate;
+        private Button btnExit;
+        private Button btnClear;
     }
 }
